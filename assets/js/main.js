89e532a5ac95910a55c5793e956b9a8c80066fc9
EliteOutrich <elite.matias@im.skubbs.com>
2021-08-24 18:23:27 +0800

    bar chart and basic progress bars

--- assets/js/main.js
+++ assets/js/main.js
@@ -369,4 +369,53 @@ function chartMethods() {
   };
   var pieChart = new Chart(jQuery('canvas#pieChart'), pie_config);
   // Pie Chart End
+
+  // Bar Chart Start
+  const bar_labels = [
+    'Column 1',
+    'Column 2',
+    'Column 3',
+    'Column 4',
+    'Column 5',
+    'Column 6'
+  ];
+  const bar_data = {
+    labels: bar_labels,
+    datasets: [{
+      label: 'Dataset 1',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
+      borderWidth: 1
+    }]
+  };
+  const bar_config = {
+    type: 'bar',
+    data: bar_data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+  var barChart = new Chart(jQuery('canvas#barChart'), bar_config);
+  // Bar Chart End
 }
